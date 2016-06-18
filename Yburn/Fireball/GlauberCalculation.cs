d42629e2ab332a6e38d67fecc9706770aca6c3ed
VietHung <dinhviethung95@yahoo.de>
2016-06-18 11:55:14 +0200

    inelastic cross section for pp collision is adjusted to higher invariant mass

--- Yburn/Fireball/GlauberCalculation.cs
+++ Yburn/Fireball/GlauberCalculation.cs
@@ -136,7 +136,8 @@ namespace Yburn.Fireball
 		private static readonly double NmixALICEFittingConstant = 0.8;
 
 		// inelastic cross section for pp collisions is 64 mb = 6.4 fm^2 at 2.76 TeV
-		private static readonly double InelasticppCrossSectionFm = 6.4;
+        // inelastic cross section for pp collisions is 68 mb = 6.8 fm^2 at 5.02 TeV
+		private static readonly double InelasticppCrossSectionFm = 6.8;
 
 		private FireballParam Param;
 
